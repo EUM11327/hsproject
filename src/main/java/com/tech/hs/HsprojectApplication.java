db2db3393690cdb18a6c91b4f8b21a0cb6b4177c
EUM11327 <gustjd11327@gmail.com>
2025-08-26 12:21:44 +0900

    첫 커밋

---
+++ src/main/java/com/tech/hs/HsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.tech.hs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/prjm33/HsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.prjm33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
